02e5b37e55a086c6cc2e6a3d5ac0612a0a9036b2
Matthias Ronge <matthias.ronge@zeutschel.de>
2020-07-31 14:30:50 +0200

    Rename 'Parent' to 'Division'

--- Kitodo/src/main/java/org/kitodo/production/services/dataformat/MetsService.java
+++ Kitodo/src/main/java/org/kitodo/production/services/dataformat/MetsService.java
@@ -33,7 +33,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.kitodo.api.MetadataEntry;
 import org.kitodo.api.dataformat.IncludedStructuralElement;
-import org.kitodo.api.dataformat.Parent;
+import org.kitodo.api.dataformat.Division;
 import org.kitodo.api.dataformat.Workpiece;
 import org.kitodo.api.dataformat.mets.MetsXmlElementAccessInterface;
 import org.kitodo.production.services.ServiceManager;
@@ -156,7 +156,7 @@ public class MetsService {
      * @return the number of tags
      */
     public static long countLogicalMetadata(Workpiece workpiece) {
-        return treeStream(workpiece.getLogicalStructureRoot(), Parent::getChildren)
+        return treeStream(workpiece.getLogicalStructureRoot(), Division::getChildren)
                 .flatMap(includedStructuralElement -> includedStructuralElement.getMetadata().parallelStream())
                 .filter(metadata -> !(metadata instanceof MetadataEntry)
                         || Objects.nonNull(((MetadataEntry) metadata).getValue())
