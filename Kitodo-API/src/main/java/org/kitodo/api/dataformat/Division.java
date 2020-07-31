56039639bb3493870e23ec5a64d8b7f64a1cbe8f
Matthias Ronge <matthias.ronge@zeutschel.de>
2020-07-31 15:18:04 +0200

    Summarize common factors from subclasses to 'Division'

--- Kitodo-API/src/main/java/org/kitodo/api/dataformat/Division.java
+++ Kitodo-API/src/main/java/org/kitodo/api/dataformat/Division.java
@@ -12,12 +12,180 @@
 package org.kitodo.api.dataformat;
 
 import java.util.Collection;
+import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Objects;
 
 import org.kitodo.api.Metadata;
 
-public interface Division<T> {
-    List<T> getChildren();
+public abstract class Division<T extends Division<T>> {
+    /**
+     * The children of this division, which form the structure tree.
+     */
+    private List<T> children = new LinkedList<>();
 
-    Collection<Metadata> getMetadata();
+    /**
+     * The metadata for this division.
+     */
+    private Collection<Metadata> metadata = new HashSet<>();
+
+    /**
+     * Sequence number. The playback order of the media units when referenced
+     * from an included structural element is determined by this attribute (not
+     * by the order of the references).
+     */
+    private int order;
+
+    /**
+     * The order label of this division.
+     *
+     * <p>
+     * <i>For included structural elements</i>, used to store <b>the
+     * machine-readable value</b> if the <i>label</i> contains a human-readable
+     * value that can be mapped to a machine-readable value.
+     *
+     * <p>
+     * <i>For media units</i>, <b>a human readable label</b> for the
+     * <i>order</i> of this media unit. This need not be directly related to the
+     * order number. Examples of order labels could be “I, II, III, IV, V,  - ,
+     * 1, 2, 3”, meanwhile the order would be “1, 2, 3, 4, 5, 6, 7, 8, 9”.
+     */
+    private String orderlabel;
+
+    /**
+     * The type of included structural element. Although the data type of this
+     * variable is a string, it is recommended to use a controlled vocabulary.
+     * If the generated METS files are to be used with the DFG Viewer, the list
+     * of possible included structural element types is defined.
+     *
+     * @see "https://dfg-viewer.de/en/structural-data-set/"
+     */
+    private String type;
+
+    /**
+     * Creates a new division.
+     */
+    protected Division() {
+    }
+
+    /**
+     * Creates a new division from an existing division. This is used by a
+     * subclass to make a division an instance of itself, so the shallow copies
+     * of {@code children} and {@code metadata} are intended.
+     *
+     * @param source
+     *            division that serves as data source
+     */
+    protected Division(Division<T> source) {
+        children = source.children;
+        metadata = source.metadata;
+        order = source.order;
+        orderlabel = source.orderlabel;
+        type = source.type;
+    }
+
+    /**
+     * Returns the children of this division.
+     *
+     * @return the children
+     */
+    public List<T> getChildren() {
+        return children;
+    }
+
+    /**
+     * Returns the metadata on this structure.
+     *
+     * @return the metadata
+     */
+    public Collection<Metadata> getMetadata() {
+        return metadata;
+    }
+
+    /**
+     * Get order.
+     *
+     * @return value of order
+     */
+    public int getOrder() {
+        return order;
+    }
+
+    /**
+     * Set order.
+     *
+     * @param order
+     *            as int
+     */
+    public void setOrder(int order) {
+        this.order = order;
+    }
+
+    /**
+     * Returns the order label for this media unit.
+     *
+     * @return the order label
+     */
+    public String getOrderlabel() {
+        return orderlabel;
+    }
+
+    /**
+     * Sets the order label for this media unit.
+     *
+     * @param orderlabel
+     *            order label to set
+     */
+    public void setOrderlabel(String orderlabel) {
+        this.orderlabel = orderlabel;
+    }
+
+    /**
+     * Returns the type of this included structural element.
+     *
+     * @return the type
+     */
+    public String getType() {
+        return type;
+    }
+
+    /**
+     * Sets the type of this included structural element.
+     *
+     * @param type
+     *            type to set
+     */
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    @Override
+    public boolean equals(Object compared) {
+        if (this == compared) {
+            return true;
+        }
+        if (compared == null) {
+            return false;
+        }
+        if (!(compared instanceof Division)) {
+            return false;
+        }
+        Division<?> other = (Division<?>) compared;
+        return Objects.equals(children, other.children) && Objects.equals(metadata, other.metadata)
+                && order == other.order && Objects.equals(orderlabel, other.orderlabel)
+                && Objects.equals(type, other.type);
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((children == null) ? 0 : children.hashCode());
+        result = prime * result + ((metadata == null) ? 0 : metadata.hashCode());
+        result = prime * result + order;
+        result = prime * result + ((orderlabel == null) ? 0 : orderlabel.hashCode());
+        result = prime * result + ((type == null) ? 0 : type.hashCode());
+        return result;
+    }
 }
--- Kitodo-API/src/main/java/org/kitodo/api/dataformat/MediaUnit.java
+++ Kitodo-API/src/main/java/org/kitodo/api/dataformat/MediaUnit.java
@@ -12,9 +12,7 @@
 package org.kitodo.api.dataformat;
 
 import java.net.URI;
-import java.util.Collection;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
@@ -22,49 +20,14 @@ import java.util.Objects;
 import java.util.stream.Collectors;
 
 import org.apache.commons.io.FilenameUtils;
-import org.kitodo.api.Metadata;
 import org.kitodo.api.dataformat.mets.KitodoUUID;
 
-public class MediaUnit implements Division<MediaUnit> {
-    /**
-     * The subordinate media units of this media unit, which form the media unit
-     * tree. The order of the substructures is subordinate media units by the
-     * order of the {@code <div>} elements in the
-     * {@code <structMap TYPE="PHYSICAL">} in the METS file.
-     */
-    private List<MediaUnit> children = new LinkedList<>();
-
+public class MediaUnit extends Division<MediaUnit> {
     /**
      * Each media unit can be available in different variants, for each of which
      * a media file is available. This is in this map.
      */
     private Map<MediaVariant, URI> mediaFiles = new HashMap<>();
-
-    /**
-     * The metadata for this media unit. This media unit can be described with
-     * any metadata.
-     */
-    private Collection<Metadata> metadata = new HashSet<>();
-
-    /**
-     * Sequence number of the media unit. The playback order of the media units
-     * when referenced from an included structural element is determined by this
-     * attribute (not by the order of the references).
-     */
-    private int order;
-
-    /**
-     * A human readable label for the order of this media unit. This need not be
-     * directly related to the order number. Examples of order labels could be
-     * “I, II, III, IV, V,  - , 1, 2, 3”, meanwhile the order would be “1, 2, 3,
-     * 4, 5, 6, 7, 8, 9”.
-     */
-    private String orderlabel;
-
-    /**
-     * The type of the media unit.
-     */
-    private String type;
 
     /**
      * Saves the METS identifier for the division.
@@ -83,17 +46,6 @@ public class MediaUnit implements Division<MediaUnit> {
         includedStructuralElements = new LinkedList<>();
     }
 
-
-    /**
-     * Returns the subordinate media units associated with this media unit.
-     *
-     * @return the subordinate media units
-     */
-    @Override
-    public List<MediaUnit> getChildren() {
-        return children;
-    }
-
     /**
      * Returns the map of available media variants with the corresponding media
      * file URIs.
@@ -102,72 +54,6 @@ public class MediaUnit implements Division<MediaUnit> {
      */
     public Map<MediaVariant, URI> getMediaFiles() {
         return mediaFiles;
-    }
-
-    /**
-     * Returns the metadata on this structure.
-     *
-     * @return the metadata
-     */
-    public Collection<Metadata> getMetadata() {
-        return metadata;
-    }
-
-    /**
-     * Returns the order number for this media unit.
-     *
-     * @return the order number
-     */
-    public int getOrder() {
-        return order;
-    }
-
-    /**
-     * Sets the order number for this media unit.
-     *
-     * @param order
-     *            order number to set
-     */
-    public void setOrder(int order) {
-        this.order = order;
-    }
-
-    /**
-     * Returns the order label for this media unit.
-     *
-     * @return the order label
-     */
-    public String getOrderlabel() {
-        return orderlabel;
-    }
-
-    /**
-     * Sets the order label for this media unit.
-     *
-     * @param orderlabel
-     *            order label to set
-     */
-    public void setOrderlabel(String orderlabel) {
-        this.orderlabel = orderlabel;
-    }
-
-    /**
-     * Returns the type of this media unit.
-     *
-     * @return the type
-     */
-    public String getType() {
-        return type;
-    }
-
-    /**
-     * Sets the type of this media unit.
-     *
-     * @param type
-     *            type to set
-     */
-    public void setType(String type) {
-        this.type = type;
     }
 
     /**
@@ -208,39 +94,33 @@ public class MediaUnit implements Division<MediaUnit> {
             URI uri = mediaFiles.entrySet().iterator().next().getValue();
             fileName = FilenameUtils.getBaseName(uri.getPath()).concat(" (");
         }
-        if (Objects.nonNull(type)) {
-            fileName = type + ' ' + fileName;
+        if (Objects.nonNull(getType())) {
+            fileName = getType() + ' ' + fileName;
         }
         return mediaFiles.keySet().stream().map(MediaVariant::getUse)
                 .collect(Collectors.joining(", ", fileName, ")"));
     }
 
     @Override
-    public boolean equals(Object o) {
-        if (this == o) {
+    public boolean equals(Object compared) {
+        if (this == compared) {
             return true;
         }
-        if (!(o instanceof MediaUnit)) {
+        if (!super.equals(compared)) {
             return false;
         }
-        MediaUnit mediaUnit = (MediaUnit) o;
-        return order == mediaUnit.order
-                && Objects.equals(children, mediaUnit.children)
-                && Objects.equals(mediaFiles, mediaUnit.mediaFiles)
-                && Objects.equals(metadata, mediaUnit.metadata)
-                && Objects.equals(orderlabel, mediaUnit.orderlabel)
-                && Objects.equals(type, mediaUnit.type);
+        if (!(compared instanceof MediaUnit)) {
+            return false;
+        }
+        MediaUnit other = (MediaUnit) compared;
+        return Objects.equals(mediaFiles, other.mediaFiles);
     }
 
     @Override
     public int hashCode() {
         final int prime = 31;
-        int hashCode = 1;
-        hashCode = prime * hashCode + ((mediaFiles == null) ? 0 : mediaFiles.hashCode());
-        hashCode = prime * hashCode + order;
-        hashCode = prime * hashCode + ((orderlabel == null) ? 0 : orderlabel.hashCode());
-        hashCode = prime * hashCode + (Objects.isNull(type) ? 0 : type.hashCode());
-        return hashCode;
+        int result = super.hashCode();
+        result = prime * result + ((mediaFiles == null) ? 0 : mediaFiles.hashCode());
+        return result;
     }
-
 }
